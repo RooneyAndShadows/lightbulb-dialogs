2fe34d75773c5be0421394690fd0c0dd2cc3049b
rooney123777 <rooney123777@gmail.com>
2022-12-31 17:35:32 +0200

    Demo work.

---
+++ app/src/main/java/com/github/rooneyandshadows/lightbulb/dialogsdemo/activity/MenuConfigurations.kt
@@ -0,0 +1,120 @@
+package com.github.rooneyandshadows.lightbulb.dialogsdemo.activity
+
+import android.annotation.SuppressLint
+import android.view.View
+import com.github.rooneyandshadows.lightbulb.application.activity.BaseActivity
+import com.github.rooneyandshadows.lightbulb.application.activity.slidermenu.SliderMenu
+import com.github.rooneyandshadows.lightbulb.application.activity.slidermenu.config.SliderMenuConfiguration
+import com.github.rooneyandshadows.lightbulb.application.activity.slidermenu.items.PrimaryMenuItem
+import com.github.rooneyandshadows.lightbulb.commons.utils.ResourceUtils
+import com.github.rooneyandshadows.lightbulb.dialogs.R
+
+object MenuConfigurations {
+    @SuppressLint("InflateParams")
+    fun getConfiguration(activity: BaseActivity): SliderMenuConfiguration {
+        val headingView: View = activity.layoutInflater.inflate(R.layout.demo_drawer_header_view, null)
+        val configuration = SliderMenuConfiguration()
+        configuration.withHeaderView(headingView)
+        configuration.addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_alert_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_loading_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_adapter_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_color_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_icon_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_date_range_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_date_time_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_month_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        ).addMenuItem(
+            PrimaryMenuItem(
+                -1,
+                ResourceUtils.getPhrase(activity, R.string.demo_time_title),
+                null,
+                null,
+                1
+            ) { slider: SliderMenu ->
+                // route().back()
+                //slider.closeSlider()
+            }
+        )
+        return configuration
+    }
+}
---
+++ app/src/main/java/com/github/rooneyandshadows/lightbulb/dialogsdemo/fragments/RegularDemoFragment.kt
@@ -0,0 +1,47 @@
+package com.github.rooneyandshadows.lightbulb.dialogsdemo.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.github.rooneyandshadows.lightbulb.annotation_processors.annotations.FragmentConfiguration
+import com.github.rooneyandshadows.lightbulb.annotation_processors.annotations.FragmentScreen
+import com.github.rooneyandshadows.lightbulb.application.activity.BaseActivity
+import com.github.rooneyandshadows.lightbulb.application.activity.slidermenu.drawable.ShowMenuDrawable
+import com.github.rooneyandshadows.lightbulb.application.fragment.base.BaseFragment
+import com.github.rooneyandshadows.lightbulb.application.fragment.cofiguration.ActionBarConfiguration
+import com.github.rooneyandshadows.lightbulb.commons.utils.ResourceUtils
+import com.github.rooneyandshadows.lightbulb.dialogs.R
+import com.github.rooneyandshadows.lightbulb.dialogsdemo.activity.MainActivity
+import com.github.rooneyandshadows.lightbulb.dialogsdemo.activity.MenuConfigurations
+
+@FragmentScreen(screenName = "Alert", screenGroup = "Demo")
+@FragmentConfiguration(layoutName = "fragment_demo_dialog_alert", hasLeftDrawer = true)
+class RegularDemoFragment : BaseFragment() {
+
+
+    @Override
+    override fun configureActionBar(): ActionBarConfiguration {
+        return ActionBarConfiguration(R.id.toolbar)
+            .withActionButtons(true)
+            .attachToDrawer(true)
+            .withSubTitle(ResourceUtils.getPhrase(requireContext(), R.string.demo_alert_title))
+            .withTitle(ResourceUtils.getPhrase(requireContext(), R.string.app_name))
+    }
+
+    @Override
+    override fun doOnViewCreated(fragmentView: View, savedInstanceState: Bundle?) {
+        if (getFragmentState() === FragmentStates.CREATED) {
+            BaseActivity.updateMenuConfiguration(
+                requireContext(),
+                MainActivity::class.java
+            ) { activity: BaseActivity -> MenuConfigurations.getConfiguration(activity) }
+        }
+        setupDrawerButton()
+    }
+
+    private fun setupDrawerButton() {
+        val actionBarDrawable = ShowMenuDrawable(requireContext())
+        actionBarDrawable.setEnabled(false)
+        actionBarDrawable.backgroundColor = ResourceUtils.getColorByAttribute(requireContext(), R.attr.colorError)
+        actionBarManager.setHomeIcon(actionBarDrawable)
+    }
+}
